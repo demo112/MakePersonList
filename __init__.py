a39606ad031e6a98f607f36614e2cfa244d33bed
demo112 <huafengdongji@hotamial.com>
2020-10-10 14:10:26 +0800

    add en001~010

--- __init__.py
+++ __init__.py
@@ -4,8 +4,8 @@ INPUTPATH = "C:\\Users\\user\\PycharmProjects\\MakePersonList\\Template"
 title_en = ["Person ID(*)", "Name(*)", "Gender(*)", "ID Card Number", "IC Card Number", "Date of Birth", "Phone", "Address"]
 title_cn = ["人员编号(*)", "姓名(*)", "性别(*)", "身份证号", "IC卡号", "出生年月", "手机号码", "地址"]
 
-# 人员配置
+# 人员配置规格
 max_person = 5000
 max_person_each_dp = 500
 
-# 
+# 人员信息配置
